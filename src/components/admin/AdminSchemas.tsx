240786e073f347706a774e790011eccfdededb94
nechostel21k4 <nechostel21k4@gmail.com>
2025-12-27 21:36:50 +0530

    updateschemas

--- src/components/admin/AdminSchemas.tsx
+++ src/components/admin/AdminSchemas.tsx
@@ -10,12 +10,16 @@ import React, { useEffect, useRef, useState } from "react";
 import {
   AddandGetAllBranches,
   AddandGetAllColleges,
+  AddandGetAllHostels,
   deleteBranchData,
   deleteCollegeData,
+  deleteHostelData,
   getAllBranches,
   getAllColleges,
+  getAllHostels,
   updateBranchData,
   updateCollegeData,
+  updateHostelData,
 } from "../../services/SchemaService";
 
 interface College {
@@ -30,9 +34,16 @@ interface Branch {
   name: string;
 }
 
+interface Hostel {
+  _id: string;
+  code: string;
+  name: string;
+}
+
 function AdminSchemas() {
   const [colleges, setColleges] = useState<College[]>([]);
   const [branches, setBranches] = useState<Branch[]>([]);
+  const [hostels, setHostels] = useState<Hostel[]>([]);
 
   const [activeID, setActiveID] = useState<string>("");
   const [isUpdating, setIsUpdating] = useState<boolean>(false);
@@ -57,6 +68,14 @@ function AdminSchemas() {
     });
   }, []);
 
+  useEffect(() => {
+    getAllHostels().then((data) => {
+      if (data?.hostels) {
+        setHostels(data?.hostels);
+      }
+    });
+  }, []);
+
   const onCollegeRowEditComplete = (e: DataTableRowEditCompleteEvent) => {
     let _colleges = [...colleges];
     let { newData, index } = e;
@@ -73,6 +92,15 @@ function AdminSchemas() {
     _branches[index] = newData as Branch;
 
     setBranches(_branches);
+  };
+
+  const onHostelRowEditComplete = (e: DataTableRowEditCompleteEvent) => {
+    let _hostels = [...hostels];
+    let { newData, index } = e;
+
+    _hostels[index] = newData as Hostel;
+
+    setHostels(_hostels);
   };
 
   const textEditor = (options: ColumnEditorOptions) => {
@@ -130,7 +158,7 @@ function AdminSchemas() {
         });
     };
 
-    const reject = () => {};
+    const reject = () => { };
 
     confirmDialog({
       message: "Are you sure you want to Update?",
@@ -176,7 +204,7 @@ function AdminSchemas() {
         });
     };
 
-    const reject = () => {};
+    const reject = () => { };
 
     confirmDialog({
       message: "Are you sure you want to Delete?",
@@ -234,23 +262,23 @@ function AdminSchemas() {
             if (data?.colleges) {
               setColleges(data?.colleges);
             }
-            
-              toast.current &&
-                toast.current.show({
-                  severity: "success",
-                  summary: "Success",
-                  detail: "New College added successfully.Please Edit",
-                });
-            
+
+            toast.current &&
+              toast.current.show({
+                severity: "success",
+                summary: "Success",
+                detail: "New College added successfully.Please Edit",
+              });
+
           } else {
-            
-              toast.current &&
-                toast.current.show({
-                  severity: "error",
-                  summary: "Failure",
-                  detail: "Failed to add new college. Try again",
-                });
-            
+
+            toast.current &&
+              toast.current.show({
+                severity: "error",
+                summary: "Failure",
+                detail: "Failed to add new college. Try again",
+              });
+
           }
         })
         .catch((err) => {
@@ -258,7 +286,7 @@ function AdminSchemas() {
         });
     };
 
-    const reject = () => {};
+    const reject = () => { };
 
     confirmDialog({
       message: "Are you sure you want to Add new College?",
@@ -302,7 +330,7 @@ function AdminSchemas() {
         });
     };
 
-    const reject = () => {};
+    const reject = () => { };
 
     confirmDialog({
       message: "Are you sure you want to Update?",
@@ -348,7 +376,7 @@ function AdminSchemas() {
         });
     };
 
-    const reject = () => {};
+    const reject = () => { };
 
     confirmDialog({
       message: "Are you sure you want to Delete?",
@@ -406,23 +434,23 @@ function AdminSchemas() {
             if (data?.branches) {
               setBranches(data?.branches);
             }
-            
-              toast.current &&
-                toast.current.show({
-                  severity: "success",
-                  summary: "Success",
-                  detail: "New Branch added successfully.Please Edit",
-                });
-            
+
+            toast.current &&
+              toast.current.show({
+                severity: "success",
+                summary: "Success",
+                detail: "New Branch added successfully.Please Edit",
+              });
+
           } else {
-            
-              toast.current &&
-                toast.current.show({
-                  severity: "error",
-                  summary: "Failure",
-                  detail: "Failed to add new branch. Try again",
-                });
-            
+
+            toast.current &&
+              toast.current.show({
+                severity: "error",
+                summary: "Failure",
+                detail: "Failed to add new branch. Try again",
+              });
+
           }
         })
         .catch((err) => {
@@ -430,10 +458,182 @@ function AdminSchemas() {
         });
     };
 
-    const reject = () => {};
+    const reject = () => { };
 
     confirmDialog({
       message: "Are you sure you want to Add new Branch?",
+      header: "Confirmation",
+      icon: "pi pi-exclamation-triangle",
+      defaultFocus: "reject",
+
+      accept,
+      reject,
+    });
+  };
+
+
+
+  const handleHostelUpdate = (data: Hostel) => {
+    const accept = () => {
+      setActiveID(data._id);
+      setIsUpdating(true);
+      updateHostelData(data, data._id)
+        .then((data) => {
+          setActiveID("");
+          setIsUpdating(false);
+          if (data?.success) {
+            toast.current &&
+              toast.current.show({
+                severity: "success",
+                summary: "Success",
+                detail: "Updated Successfully !",
+              });
+          } else {
+            toast.current &&
+              toast.current.show({
+                severity: "error",
+                summary: "Failure",
+                detail: "Failed to Update.Try again",
+              });
+          }
+        })
+        .catch((err) => {
+          console.log("Error : while updating hostel data", err);
+        });
+    };
+
+    const reject = () => { };
+
+    confirmDialog({
+      message: "Are you sure you want to Update?",
+      header: "Confirmation",
+      icon: "pi pi-exclamation-triangle",
+      defaultFocus: "reject",
+      accept,
+      reject,
+    });
+  };
+
+  const handleHostelDelete = (data: Hostel) => {
+    const accept = () => {
+      setActiveID(data._id);
+      setIsDeleting(true);
+      deleteHostelData(data._id)
+        .then((result) => {
+          setActiveID("");
+          setIsDeleting(false);
+          if (result?.success) {
+            let newHostels = hostels.filter(
+              (hostel: Hostel) => hostel._id !== data._id
+            );
+            setHostels(newHostels);
+
+            toast.current &&
+              toast.current.show({
+                severity: "success",
+                summary: "Success",
+                detail: "Deleted Successfully !",
+              });
+          } else {
+            toast.current &&
+              toast.current.show({
+                severity: "error",
+                summary: "Failure",
+                detail: "Failed to Delete.Try again",
+              });
+          }
+        })
+        .catch((err) => {
+          console.log("Error : while deleting hostel data", err);
+        });
+    };
+
+    const reject = () => { };
+
+    confirmDialog({
+      message: "Are you sure you want to Delete?",
+      header: "Confirmation",
+      icon: "pi pi-exclamation-triangle",
+      defaultFocus: "reject",
+      accept,
+      reject,
+    });
+  };
+
+  const HostelUpdateButton = (data: Hostel) => {
+    return (
+      <>
+        <Button
+          label={isUpdating && activeID === data._id ? "Updating" : "Update"}
+          icon={
+            isUpdating && activeID === data._id
+              ? "pi pi-spin pi-spinner"
+              : "pi pi-save"
+          }
+          severity="success"
+          onClick={() => {
+            handleHostelUpdate(data);
+          }}
+        ></Button>
+      </>
+    );
+  };
+
+  const HostelDeleteButton = (data: Hostel) => {
+    return (
+      <>
+        <Button
+          label={isDeleting && activeID === data._id ? "Deleting" : "Delete"}
+          icon={
+            isDeleting && activeID === data._id
+              ? "pi pi-spin pi-spinner"
+              : "pi pi-trash"
+          }
+          severity="danger"
+          onClick={() => {
+            handleHostelDelete(data);
+          }}
+        ></Button>
+      </>
+    );
+  };
+
+  const handleAddHostel = () => {
+    const accept = () => {
+      AddandGetAllHostels()
+        .then((data) => {
+          if (data?.success) {
+            if (data?.hostels) {
+              setHostels(data?.hostels);
+            }
+
+            toast.current &&
+              toast.current.show({
+                severity: "success",
+                summary: "Success",
+                detail: "New Hostel added successfully.Please Edit",
+              });
+
+          } else {
+
+            toast.current &&
+              toast.current.show({
+                severity: "error",
+                summary: "Failure",
+                detail: "Failed to add new hostel. Try again",
+              });
+
+          }
+        })
+        .catch((err) => {
+          console.log("Error : while getting all hostel names", err);
+        });
+    };
+
+    const reject = () => { };
+
+    confirmDialog({
+      message: "Are you sure you want to Add new Hostel?",
       header: "Confirmation",
       icon: "pi pi-exclamation-triangle",
       defaultFocus: "reject",
@@ -527,6 +727,43 @@ function AdminSchemas() {
             <Button
               label="New"
               onClick={handleAddBranch}
+              icon="pi pi-plus-circle"
+            ></Button>
+          </div>
+        </Card>
+
+        <Card title="Hostel Schema" className="special-font mt-3 mb-3">
+          <DataTable
+            value={hostels}
+            stripedRows
+            selectionMode={"single"}
+            editMode="row"
+            dataKey="_id"
+            onRowEditComplete={onHostelRowEditComplete}
+            tableStyle={{ minWidth: "50rem" }}
+          >
+            <Column
+              field="code"
+              header="Hostel Code"
+              editor={(options) => textEditorUppercase(options)}
+            ></Column>
+            <Column
+              field="name"
+              header="Hostel Name"
+              editor={(options) => textEditor(options)}
+            ></Column>
+            <Column
+              rowEditor
+              headerStyle={{ width: "10%", minWidth: "8rem" }}
+              bodyStyle={{ textAlign: "center" }}
+            ></Column>
+            <Column body={HostelUpdateButton}></Column>
+            <Column body={HostelDeleteButton}></Column>
+          </DataTable>
+          <div className="mt-3">
+            <Button
+              label="New"
+              onClick={handleAddHostel}
               icon="pi pi-plus-circle"
             ></Button>
           </div>
--- src/services/SchemaService.ts
+++ src/services/SchemaService.ts
@@ -3,79 +3,114 @@ import api from "../utils/Api";
 const server = process.env.REACT_APP_SERVER;
 
 export const getAllColleges = async () => {
-    try {
-      const response = await api.get(`${server}/schemas/getColleges`);
-      return response.data;
-    } catch (error) {
-      console.log("Error : while fetching All college names ", error);
-    }
-  };
+  try {
+    const response = await api.get(`${server}/schemas/getColleges`);
+    return response.data;
+  } catch (error) {
+    console.log("Error : while fetching All college names ", error);
+  }
+};
 
-  export const AddandGetAllColleges = async () => {
-    try {
-      const response = await api.get(`${server}/schemas/addGetColleges`);
-      return response.data;
-    } catch (error) {
-      console.log("Error : while adding and fetching All college names ", error);
-    }
-  };
+export const AddandGetAllColleges = async () => {
+  try {
+    const response = await api.get(`${server}/schemas/addGetColleges`);
+    return response.data;
+  } catch (error) {
+    console.log("Error : while adding and fetching All college names ", error);
+  }
+};
 
-  export const updateCollegeData = async (data:any,id:string) => {
-    try {
-      const response = await api.post(`${server}/schemas/updateCollegeById/${id}`,data);
-      return response.data;
-    } catch (error) {
-      console.log("Error : while Updating college data ", error);
-    }
-  };
-
-
-  export const deleteCollegeData = async (id:string) => {
-    try {
-      const response = await api.delete(`${server}/schemas/deleteCollegeById/${id}`);
-      return response.data;
-    } catch (error) {
-      console.log("Error : while Deleting college data ", error);
-    }
-  };
+export const updateCollegeData = async (data: any, id: string) => {
+  try {
+    const response = await api.post(`${server}/schemas/updateCollegeById/${id}`, data);
+    return response.data;
+  } catch (error) {
+    console.log("Error : while Updating college data ", error);
+  }
+};
 
 
-
-  export const getAllBranches = async () => {
-    try {
-      const response = await api.get(`${server}/schemas/getBranches`);
-      return response.data;
-    } catch (error) {
-      console.log("Error : while fetching All Branch names ", error);
-    }
-  };
-
-  export const AddandGetAllBranches = async () => {
-    try {
-      const response = await api.get(`${server}/schemas/addGetBranches`);
-      return response.data;
-    } catch (error) {
-      console.log("Error : while adding and fetching All branch names ", error);
-    }
-  };
-
-  export const updateBranchData = async (data:any,id:string) => {
-    try {
-      const response = await api.post(`${server}/schemas/updateBranchById/${id}`,data);
-      return response.data;
-    } catch (error) {
-      console.log("Error : while Updating branch data ", error);
-    }
-  };
+export const deleteCollegeData = async (id: string) => {
+  try {
+    const response = await api.delete(`${server}/schemas/deleteCollegeById/${id}`);
+    return response.data;
+  } catch (error) {
+    console.log("Error : while Deleting college data ", error);
+  }
+};
 
 
-  export const deleteBranchData = async (id:string) => {
-    try {
-      const response = await api.delete(`${server}/schemas/deleteBranchById/${id}`);
-      return response.data;
-    } catch (error) {
-      console.log("Error : while Deleting branch data ", error);
-    }
-  };
 
-  
+export const getAllBranches = async () => {
+  try {
+    const response = await api.get(`${server}/schemas/getBranches`);
+    return response.data;
+  } catch (error) {
+    console.log("Error : while fetching All Branch names ", error);
+  }
+};
+
+export const AddandGetAllBranches = async () => {
+  try {
+    const response = await api.get(`${server}/schemas/addGetBranches`);
+    return response.data;
+  } catch (error) {
+    console.log("Error : while adding and fetching All branch names ", error);
+  }
+};
+
+export const updateBranchData = async (data: any, id: string) => {
+  try {
+    const response = await api.post(`${server}/schemas/updateBranchById/${id}`, data);
+    return response.data;
+  } catch (error) {
+    console.log("Error : while Updating branch data ", error);
+  }
+};
+
+
+export const deleteBranchData = async (id: string) => {
+  try {
+    const response = await api.delete(`${server}/schemas/deleteBranchById/${id}`);
+    return response.data;
+  } catch (error) {
+    console.log("Error : while Deleting branch data ", error);
+  }
+};
+
+export const getAllHostels = async () => {
+  try {
+    const response = await api.get(`${server}/schemas/getHostels`);
+    return response.data;
+  } catch (error) {
+    console.log("Error : while fetching All Hostel names ", error);
+  }
+};
+
+export const AddandGetAllHostels = async () => {
+  try {
+    const response = await api.get(`${server}/schemas/addGetHostels`);
+    return response.data;
+  } catch (error) {
+    console.log("Error : while adding and fetching All hostel names ", error);
+  }
+};
+
+export const updateHostelData = async (data: any, id: string) => {
+  try {
+    const response = await api.post(`${server}/schemas/updateHostelById/${id}`, data);
+    return response.data;
+  } catch (error) {
+    console.log("Error : while Updating hostel data ", error);
+  }
+};
+
+export const deleteHostelData = async (id: string) => {
+  try {
+    const response = await api.delete(`${server}/schemas/deleteHostelById/${id}`);
+    return response.data;
+  } catch (error) {
+    console.log("Error : while Deleting hostel data ", error);
+  }
+};
+
